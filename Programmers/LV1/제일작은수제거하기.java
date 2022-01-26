a0b5a6cfa37ff6a3ce19b1f29b890cdedb1aae2d
Sung-Minseok <sms8189@gmail.com>
2022-01-26 20:13:27 +0900

    [PGS] 리스트,배열 / lv1 / 20분

---
+++ Programmers/LV1/제일작은수제거하기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] solution(int[] arr) {
+        if(arr.length == 1){
+            int a = new int[1];
+            a[0] = -1;
+            return a;
+        }
+        
+        int[] answer = {};
+        return answer;
+    }
+}
