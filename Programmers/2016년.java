6bf4c80779755204f090848e4668fd5c95a50c7a
minseok <alstjrsung@gmail.com>
2022-01-31 03:43:22 +0900

    [PGS] 배열 / lv1 / 5분

--- Programmers/2016년.java
+++ Programmers/2016년.java
@@ -1,14 +1,14 @@
 import java.util.*;
 class Solution {
     public String solution(int a, int b) {
-        String[] day = {"SUN","MON","TUE","WED","THU","FRI","SAT"};
+        String[] day = {"FRI","SAT","SUN","MON","TUE","WED","THU"};
         int[] month = {31,29,31,30,31,30,31,31,30,31,30,31};
         int result = 0;
-        for(int i=0; i<a; i++){
+        for(int i=0; i<a-1; i++){
             result +=month[i];
         }
         result+=b-1;
-        int week = (result%7)+2;
+        int week = (result%7);
         String answer = day[week];
         return answer;
     }
